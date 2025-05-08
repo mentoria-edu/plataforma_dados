d3feba6f205a08e584f457718d6cea54ed3a99c0
Leo-Adelmo <leonardo-adelmo@hotmail.com>
2025-05-08 20:29:42 -0300

    Ajustes de formatação e criação do start-platform.sh

--- start_platform.sh
+++ start_platform.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
-
-1 - Criar a imagem a partir do dockerfile base
+echo "Verificando a instalação do Docker e do docker-compose"
+if docker --version > /dev/null && docker compose --version > /dev/null; then
+    docker build -f ./hadoop_platform/docker/base.Dockerfile -t "hadoop_platform" .
+    docker compose -f ./hadoop_platform/compose/docker-compose.yaml up -d --build
+else
+    echo "Não há nenhuma versão de Docker ou do Docker compose instalado!"
+    exit 1
+fi
